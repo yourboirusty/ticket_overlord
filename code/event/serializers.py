eef7facf5bbd63823fbbeea449bf73e9cf7b40d2
Kornel Koszela <kornel.koszela@gmail.com>
2020-10-26 11:37:08 +0100

    Changed event date to datetime Expanded serializers and viewsets for event app

--- code/event/serializers.py
+++ code/event/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 from event.models import Event, AvailableTickets, Reservation
 
@@ -7,22 +7,38 @@ class TicketStatusSerializer(ModelSerializer):
     class Meta:
         model = AvailableTickets
         name = 'available_tickets'
-        fields = ('ticket_type', 'price', 'amount', 'available')
+        fields = ('id', 'ticket_type', 'price', 'amount', 'available')
 
 
 class EventSerializer(ModelSerializer):
-    tickets = TicketStatusSerializer(many=True,
-                                     read_only=True)
-    
     class Meta:
         model = Event
         name = 'event'
         fields = ('id', 'name', 'date', 'tickets')
 
 
+class DetailedEventSerializer(ModelSerializer):
+    tickets = TicketStatusSerializer(many=True)
+    class Meta:
+        model = Event
+        name = 'event'
+        fields = ('id', 'name', 'date', 'tickets')
+
 class ReservationSerializer(ModelSerializer):
 
     class Meta:
         model = Reservation
         name = 'reservation'
-        fields = ('client', 'created', 'ticket_type', 'amount')
+        fields = ('id', 'created', 'ticket_type', 'amount')
+
+    def validate(self, data):
+        user = self.context['request'].user
+        ticket = data['ticket_type']
+        q = Reservation.objects.filter(client=user, ticket_type=ticket)
+        if q.count() != 0:
+            raise ValidationError("Reservation has already been made")
+
+    def create(self, validated_data):
+        validated_data['client'] = self.context['request'].user
+        obj = Reservation.objects.create(**validated_data)
+        return obj
